130a4cf1d8e45994379b4f9bce77969e8d117bbf
Simar <125185886+SkullKnight9@users.noreply.github.com>
2023-02-12 16:18:01 +0530

    hi
    hi

--- Jan 2023/hi.py
+++ Jan 2023/hi.py
@@ -1,4 +1,29 @@
-i = 5
+print("Hi")
+print("hello my name is Simar")
 
-for i == 5:
-  print("Hi Seger")
+
+name1 = input('Hi,Who are you ? ')
+print('Welcome',name1)
+
+age = input("What is your age ?")
+print(type(age))
+age = int(age)
+print(type(age))    
+
+#try and except code
+
+name = "hello Bob"
+try:
+    name = int(name)
+except:
+    name = -1
+
+print("Hello",name)
+
+name2 = '123'
+try:
+    name2 = int(name2)
+except:
+    name2 = -1
+
+print("Hello",name2)
